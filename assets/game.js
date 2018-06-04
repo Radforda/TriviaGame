8b547401985c3a0bab9c6e806caf1abf695dd606
Andrea Radford <radfordandrealynn@gmail.com>
2018-06-04 19:59:52 -0400

    file structure and first html

---
+++ assets/game.js
@@ -0,0 +1,19 @@
+//Quesiton Objects
+
+let question1={
+    question: "What dog beed was used as a sled dog?",
+    answer1:"huskey",
+    answer2:"Poodle",
+    answer3:"Great Dane",
+    answer1ID:"correct",
+    answer2ID:"wronge1",
+    answer3ID:"wronge2",
+    videoSRC:"assets/videos/dogsRunning.mp4"
+}
+
+
+
+//Functions
+
+
+
